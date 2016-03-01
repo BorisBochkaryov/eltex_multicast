7c98ea02f7105c486307bdb3c4c81291c125cf10
Boris Bochkaryov <Boris-Bochkaryov@yandex.ru>
2016-03-01 22:09:06 +0600

    first commit

--- client.c
+++ client.c
@@ -5,47 +5,38 @@
 #include <stdlib.h>
 
 int main(){
-    int sock;
-    if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-      printf("Socket error!\n");
-      return -1;
-    }
-    // для отправки серверу
-    struct sockaddr_in addr_in;
-    addr_in.sin_family = AF_INET;
-    addr_in.sin_port = htons(7777);
-    addr_in.sin_addr.s_addr = inet_addr("192.168.0.105");
-    // для приема
-    struct sockaddr_in addr_out;
-    addr_out.sin_family = AF_INET;
-    addr_out.sin_port = htons(7777);
-    addr_out.sin_addr.s_addr = inet_addr("192.168.0.255");
-    if(bind(sock, (struct sockaddr *)&addr_out, sizeof(addr_out)) < 0){
-      printf("Error bind!\n");
-      return -1;
-    }
-    int yes;
-    // задаем тип сокета - BROADCAST
-    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (void*)&yes, sizeof(yes));
+  int sock;
+  // создаем сокет
+  if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+    printf("Socket error!\n");
+    return -1;
+  }
+  int yes;
+  // задаем тип сокета (для использования того же адреса/порта)
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void*)&yes, sizeof(yes));
+  // структура для bind
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(2016);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+    printf("Error bind!\n");
+    return -1;
+  }
+  // структура multicast
+  struct ip_mreq mr;
+  mr.imr_multiaddr.s_addr = inet_addr("225.0.0.21"); // multicast адрес
+  mr.imr_interface.s_addr = htonl(INADDR_ANY); // интерфейс
 
-    int bytes_read;
-    switch(fork()){
-      case 0: { // считывающий поток
-        while(1){
-          char buf[512] = "";
-          scanf("%s",buf);
-          sendto(sock, buf, sizeof(buf), 0, (struct sockaddr*)&addr_in, sizeof(addr_in)); // отправка серверу
-        }
-      } break;
-      default: { // получающий и выводящий поток
-        while(1){
-          char buf[512] = "";
-          int addrlen = sizeof(addr_out);
-          bytes_read = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&addr_out, &addrlen); // прием от клиентов
-          printf("Сообщение: %s\n",buf);
-        }
-      } break;
-    }
-    close(sock);
-    return 0;
+  // задаем тип сокеты (член multicast группы)
+  setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mr, sizeof(mr));
+  
+  while(1){
+    char buf[512] = "";
+    int addrlen = sizeof(addr);
+    int bytes_read = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&addr, &addrlen); // принимаем данные с multicast
+    printf(">%s\n",buf); // выводим принятую информацию
+  }
+  close(sock);
+  return 0;
 }
--- server.c
+++ server.c
@@ -6,44 +6,23 @@
 #include <string.h>
 
 int main(){
-    int sock;
-    if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-      printf("Socket error!\n");
-      return -1;
-    }
-    // для приема
-    struct sockaddr_in addr_in;
-    addr_in.sin_family = AF_INET;
-    addr_in.sin_port = htons(7777);
-    addr_in.sin_addr.s_addr = htonl(INADDR_ANY);
-    if(bind(sock, (struct sockaddr *)&addr_in, sizeof(addr_in)) < 0){
-      printf("Error bind!\n");
-      return -1;
-    }
-    // для отправки
-    struct sockaddr_in addr_out;
-    addr_out.sin_family = AF_INET;
-    addr_out.sin_port = htons(7777);
-    addr_out.sin_addr.s_addr = inet_addr("192.168.0.255");
+  int sock;
+  // создаем сокет
+  if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+    printf("Socket error!\n");
+    return -1;
+  }
+  // multicast адрес
+  struct sockaddr_in addr_m;
+  addr_m.sin_family = AF_INET;
+  addr_m.sin_port = htons(2016);
+  addr_m.sin_addr.s_addr = inet_addr("225.0.0.21");
 
-    int yes;
-    // тип сокета - BROADCAST
-    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (void *) &yes, sizeof(yes));
-
-    int bytes_read;
-    char buf[512] = "", str[512] = "";
-    while(1){
-      buf[512] = "",str[512] = "";
-      int addrlen = sizeof(addr_in);
-      // принимаем
-      bytes_read = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&addr_in, &addrlen); // прием от клиентов
-      if(buf[0] != '@'){ // если это сообщение не от сервера
-        sprintf(str,"@%s",buf); // дописываем символ
-	printf("%s",str);
-        // отправляем клиентам
-        sendto(sock, str, sizeof(str), 0, (struct sockaddr*)&addr_out, sizeof(addr_out)); // отправка всем клиентам
-      }
-    }
-    close(sock);
-    return 0;
+  char msg[512] = "Hello, World!";
+  while(1){
+    sendto(sock, msg, sizeof(msg), 0, (struct sockaddr*)&addr_m, sizeof(addr_m)); // отправляем на multicast
+    sleep(2); // пауза
+  }
+  close(sock);
+  return 0;
 }
